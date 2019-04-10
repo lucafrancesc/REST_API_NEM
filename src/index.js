c6b21583ce40f1d800ddce291319ea5b7263e774
Luca <eto.lucafrancesco@gmail.com>
2019-04-10 18:00:51 +0100

    CORS basic setup. Will dive in later.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 /*jshint esversion: 6 */
 
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!')
+  console.log(`Example app listening on port 3000!`)
 );
