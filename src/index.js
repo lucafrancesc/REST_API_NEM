ca3a1547d17d8b9d1826454c1d1846fea225a822
Luca <eto.lucafrancesco@gmail.com>
2019-04-11 11:35:13 +0100

    Move routes into a separeted folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
 import 'dotenv/config';
 import express from 'express';
-import uuidv4 from 'uuid/v4';
 import bodyParser from 'body-parser';
 import models from './models';
+import routes from './routes';
 
 const app = express();
 
@@ -17,49 +16,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/session', (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-});
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete('/messages/:messageId', (req, res) => {
-  const {
-    [req.params.messageId]: message,
-    ...otherMessages
-  } = req.context.models.messages;
-
-  req.context.models.messages = otherMessages;
-
-  return res.send(message);
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(3000, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
